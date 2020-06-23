20c143661206612f534f635764f43b50d259dcb8
Pedro Leandro <pedroprofile@gmail.com>
2020-06-23 14:22:04 -0300

    Configuração do redux e rotas, criação do menu

---
+++ frontend/src/components/partials/Header/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { HeaderArea } from './styled';
+
+import { isLogged } from '../../../helpers/AuthHandler';
+
+const Header = () => {
+    let logged = isLogged();
+
+    return (
+        <HeaderArea>
+            <div className="container">
+                 <div className="logo">
+                    <span className="logo-1">O</span>
+                    <span className="logo-2">L</span>
+                    <span className="logo-3">X</span>
+                 </div>
+
+                 <nav>
+                     <ul>   
+                        {logged &&
+                            <>
+                                <li>
+                                    <Link to="/signin">Minha Conta</Link>
+                                </li>
+                                <li>
+                                    <Link to="/loggout">Sair</Link>
+                                </li>
+                                <li>
+                                    <Link to="/post-an-ad" className="button" >Poste um anúncio</Link>
+                                </li>
+                            </>
+                        }
+
+                        {!logged &&
+                            <>
+                                <li>
+                                    <Link to="/signin">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signup">Cadastrar</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signin" className="button" >Poste um anúncio</Link>
+                                </li>
+                            </>
+                        }
+                     </ul>
+                 </nav>
+            </div>
+        </HeaderArea>
+    );
+}
+
+export default Header;
