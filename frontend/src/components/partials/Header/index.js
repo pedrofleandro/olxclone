1400e80c78b187a252c43a28fccbeaf300341410
Pedro Leandro <pedroprofile@gmail.com>
2020-06-30 12:20:08 -0300

    Funcionalidade de Login e Cadastro no sistema

--- frontend/src/components/partials/Header/index.js
+++ frontend/src/components/partials/Header/index.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HeaderArea } from './styled';
 
-import { isLogged } from '../../../helpers/AuthHandler';
+import { isLogged, doLogout } from '../../../helpers/AuthHandler';
 
 const Header = () => {
     let logged = isLogged();
+
+    const handleLogout = () => {
+        doLogout();
+        window.location.href = '/';
+
+    }
 
     return (
         <HeaderArea>
             <div className="container">
                  <div className="logo">
-                    <span className="logo-1">O</span>
-                    <span className="logo-2">L</span>
-                    <span className="logo-3">X</span>
+                    <Link to='/'>
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">X</span>
+                    </Link>
                  </div>
 
                  <nav>
@@ -21,10 +29,10 @@ const Header = () => {
                         {logged &&
                             <>
                                 <li>
-                                    <Link to="/signin">Minha Conta</Link>
+                                    <Link to="/my-account">Minha Conta</Link>
                                 </li>
                                 <li>
-                                    <Link to="/loggout">Sair</Link>
+                                    <button onClick={handleLogout}>Sair</button>
                                 </li>
                                 <li>
                                     <Link to="/post-an-ad" className="button" >Poste um anúncio</Link>
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { SearchArea, PageArea } from './styled';
+import useApi from '../../helpers/OlxAPI';
+
+
+import { PageContainer } from '../../components/MainComponents';
 
 const Page = () => {
+    const api = useApi();
+
+    const [stateList, setStateList] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [adList, setAdList] = useState([]);
+
+
+    useEffect(()=>{
+        const getStates = async () => {
+            const slist = await api.getStates();
+            setStateList(slist);
+        }
+        getStates();
+    }, []);
+
+    useEffect(()=>{
+        const getCategories = async () => {
+            const cats = await api.getCategories();
+            setCategories(cats);
+        }
+        getCategories();
+    }, []);
+
+    useEffect(()=>{
+        const getRecentAds = async () => {
+            const json = await api.getAds({
+                sort:'desc',
+                limit:8
+            });
+            setAdList(json.ads);
+        }
+        getRecentAds();
+    }, []);
+
+
+
     return (
-        <div>Página Inicial</div>
-    );
+        <>
+            <SearchArea>
+                <PageContainer>
+                    <div className="searchBox">
+                        <form method="GET" action="/ads">
+                            <input type="text" name="q" placeholder="O que você procura?" />
+                            <select name="state">
+                                {stateList.map((i, k)=>
+                                    <option key={k} value={i.name}>{i.name}</option>
+                                )}
+                            </select>
+                            <button>Pesquisar</button>
+                        </form>
+                    </div>
+                    <div class="categoryList">
+                        {categories.map((i,k) => 
+                            <Link key={k} to={`/ads?cat=${i.slug}`} className="categoryItem">
+                                <img src={i.img} alt="" />
+                                <span>{i.name}</span>
+                            </Link>
+                        )}
+                    </div>
+                </PageContainer>
+            </SearchArea>
+
+            <PageContainer>
+                <PageArea>
+                    ...
+                </PageArea>
+            </PageContainer>
+        </>
+    )
 }
 
 export default Page;
