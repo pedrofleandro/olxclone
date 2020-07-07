273e78ff34fb15f06f371120947c14ca1fab4be3
Pedro Leandro <pedroprofile@gmail.com>
2020-07-07 13:40:33 -0300

    Responsividade para mobile em todas as telas

--- frontend/src/pages/AdPage/styled.js
+++ frontend/src/pages/AdPage/styled.js
@@ -99,6 +99,43 @@ margin-top: 20px;
         color: #999;
         margin-top: 10px;
     }
+
+}
+
+@media (max-width:600px) {
+    & {
+        flex-direction: column;
+    }
+    .leftSide {
+        margin: 0;
+
+        .box {
+            width: 320px;
+            flex-direction: column;
+            margin: auto;
+        }
+
+        .adInfo {
+            padding: 10px;
+        }
+    }
+
+    .rightSide {
+        width: auto;
+        margin-top: 20px;
+
+        .box {
+            width: 320px;
+            margin: auto;
+        }
+
+        .contactSellerLink {
+            width: 320px;
+            margin: 20px auto;
+        }
+
+
+    }
 }
 
 `;
@@ -110,12 +147,24 @@ export const OthersArea = styled.div`
 
     .list {
         display: flex;
+        flex-wrap: wrap;
 
         .aditem {
             width: 25%;
         }
 
     }
+
+    @media (max-width: 600px) {
+        & {
+            margin: 10px;
+        }
+
+        .list .aditem {
+            width: 50%;
+        }
+    }
+
 `;
 
 export const BreadChumb = styled.div`
@@ -127,6 +176,12 @@ a {
     margin: 0px 5px;
     text-decoration: underline;
     color: #000
+}
+
+@media (max-width: 600px) {
+    & {
+        margin: 20px;
+    }
 }
 `;
 
